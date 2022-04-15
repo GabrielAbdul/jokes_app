bb8340a0e1b9c668f878f7c186deb693fe4d325c
Gabriel Abdul <Gabrielabdul1@gmail.com>
2022-04-15 09:05:19 -0500

    Add project files.

---
+++ jokes_app/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using jokes_app.Models;
+
+namespace jokes_app.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<jokes_app.Models.Joke> Joke { get; set; }
+    }
+}
